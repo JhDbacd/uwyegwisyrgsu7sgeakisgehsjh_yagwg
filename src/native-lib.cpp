5dce242a826e15842408f56a14df2daaf59e8f93
JhDbacd <lorraime89@gmail.com>
2024-11-16 20:26:11 -0300

    Create native-lib.cpp

---
+++ src/native-lib.cpp
@@ -0,0 +1,25 @@
+#include <jni.h>
+#include <android/log.h>
+
+#define LOGI(tag, ...) __android_log_print(ANDROID_LOG_INFO, tag, __VA_ARGS__)
+
+void* startModMenuThread(JavaVM* vm) {
+    JNIEnv* env;
+    
+    if (vm->AttachCurrentThread(reinterpret_,cast<void**>(&env)) != JNI_OK) {
+        return JNI_ERR;
+    }
+    
+    
+}
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+    
+    if (vm->GetEnv(reinterpret_,cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return JNI_ERR;
+    }
+
+    LOGI("LOAD::", "Sucesso no carregamento");
+    return JNI_VERSION_1_6;
+}

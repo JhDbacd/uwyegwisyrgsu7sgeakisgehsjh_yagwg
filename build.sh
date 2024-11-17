422dc6c66795df4f3ed05b334dd21d27d9f971bd
JhDbacd <lorraime89@gmail.com>
2024-11-17 12:09:12 -0300

    Update and rename config.sh to build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O https://dl.google.com/android/repository/android-ndk-r27c-linux.zi
+unzip android-ndk-r27c-linux
+export ANDROID_NDK=$HOME/android-ndk-r27c-linux
+curl -O https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-x86_64.sh
+bash cmake-3.31.0-linux-x86_64.sh
--- config.sh
+++
@@ -1,1 +0,0 @@
-curl -O https://dl.google.com/android/repository/android-ndk-r27c-linux.zi

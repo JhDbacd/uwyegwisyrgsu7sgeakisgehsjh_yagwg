65a294c81ba21fe38ec986732f39471a1f184160
JhDbacd <lorraime89@gmail.com>
2024-11-17 14:02:35 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,15 +7,16 @@ export ANDROID_NDK=./android-ndk-r27c
 curl -O https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-x86_64.sh > /dev/null
 bash cmake-3.31.0-linux-x86_64.sh > /dev/null
 sudo apt-get update && sudo apt-get install -y build-essential 
-mkdir -p build/realeses/armeabi-v7a
+mkdir -p build/releases/armeabi-v7a
 ls $ANDROIR_NDK
 cmake -H. \
--B./build/realeses/armeabi-v7a \
+-B./build/releases/armeabi-v7a \
 -DANDROID_ABI=armeabi-v7a \
 -DANDROID_PLATFORM=android-30 \
+-DCMAKE_BUILD_TYPE=Release
 -DANDROID_NDK=$ANDROID_NDK \
 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-cd build/realeses/armeabi-v7a
+cd build/releases/armeabi-v7a
 make clean
 make 
 ls 

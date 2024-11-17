81c699ab85132ac62a7ece4b736ee3153958b028
JhDbacd <lorraime89@gmail.com>
2024-11-17 12:11:19 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -O https://dl.google.com/android/repository/android-ndk-r27c-linux.zi
+curl -O https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
 unzip android-ndk-r27c-linux
 export ANDROID_NDK=$HOME/android-ndk-r27c-linux
 curl -O https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-x86_64.sh
